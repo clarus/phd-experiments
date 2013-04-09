909f9577946ab2e5e475e6ebf11346f45c603e88
Guillaume Claret <guillaume@claret.me>
2013-04-09 16:29:18 +0200

    Monads: compose with a simpler interface

--- implicit-monads/TypeClasses.v
+++ implicit-monads/TypeClasses.v
@@ -27,7 +27,9 @@ Arguments app {M1 M2 M3 M4 monad1 monad2 monad3 monad4 f14 f24 f34} [T1 T2] e1 e
   : M3 T2 :=
   . *)
 
-Instance Id : Monad (fun A => A) := {
+Definition id := fun (A : Type) => A.
+
+Instance Id : Monad id := {
   ret A x := x;
   bind A B x f := f x }.
 
@@ -39,27 +41,18 @@ Instance Option : Monad option := {
     | None => None
     end}.
 
-Instance AnyOfId {M} {monad : Monad M} : Morphism (fun A => A) M := {
+Instance AnyOfId {M} {monad : Monad M} : Morphism id M := {
   lift A x := ret x}.
 
 Instance AnyOfAny {M} {monad : Monad M} : Morphism M M := {
   lift A x := x}.
 
-Definition bot A (x : A) : (fun A => A) A := ret (M := fun A => A) x.
+Definition bot A (x : A) : id A := ret (M := id) x.
 
 (** Examples *)
 Definition test1 := app (bot (fun n => Some (n + 1))) (Some 23).
 
 Compute test1.
-
-Definition compose A B C (f : B -> C) (g : A -> B) x := f (g x).
-
-Definition compose' {M1 M2 M3 : Type -> Type}
-  {monad1 : Monad M1} {monad2 : Monad M2} {monad3 : Monad M3}
-  {f13 : Morphism M1 M3} {f23 : Morphism M2 M3}
-  (A B C : Type) (f : B -> M2 C) (g : A -> M1 B) (x : A)
-  : M3 C :=
-  app (M1 := fun A => A) (M2 := M1) (bot f) (app (bot g) (bot x)).
 
 Definition decr (n : nat) : option nat :=
   match n with
@@ -72,16 +65,22 @@ Definition test2 := fun n => app (bot decr) (app (bot decr) (bot n)).
 Compute test2 12.
 Compute test2 1.
 
-Definition compose'' A B C :=
-  bot (fun x => bot (fun y => app (M1 := fun A => A) (M3 := fun A => A) (app (M1 := fun A => A)
-    (compose (A := A) (B := B) (C := C)) (bot x)) (bot y))).
+Definition compose A B C (f : B -> C) (g : A -> B) x := f (g x).
 
-Definition test3 : nat -> option nat :=
-  app (M1 := fun A => A) (M3 := fun A => A) (M4 := fun A => A)
-    (app (M2 := fun A => A) (M3 := fun A => A) (M4 := fun A => A) (bot (fun x => bot (fun y => app (M1 := fun A => A) (M3 := fun A => A) (app (M1 := fun A => A) compose (bot x)) (bot y)))) (bot decr)) (bot decr).
+Definition compose' {M} {monad : Monad M}
+  (A B C : Type) (f : B -> M C) (g : A -> M B) (x : A) : M C :=
+  app (bot f) (app (bot g) (bot x)).
 
-Definition nat_app (f : nat -> option nat) : option nat :=
-  f 12.
+Definition test3 :=
+  app (M1 := id) (M2 := id) (M3 := id) (M4 := id)
+    (app (M1 := id) (M2 := id) (compose' _) decr)
+    decr.
 
-Definition test2 : option nat := app (M1 := fun A => A) (M2 := fun A => A)
-  (bot nat_app) (bot (fun x => app (fun x => bot x) (bot x))).
+Compute test3 12.
+Compute test3 1.
+
+Definition identity {M} {monad : Monad M} A (x : A) : M A := ret x.
+
+Definition iapp (f : nat -> option nat) : option nat := f 12.
+
+Definition test4 := app (M1 := id) (M2 := id) iapp (identity (A := _)).
