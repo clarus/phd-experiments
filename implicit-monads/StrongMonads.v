e052a3aa85e2d51d6ed225297ef38151ce9ac509
Guillaume Claret <guillaume@claret.me>
2013-04-11 16:41:35 +0200

    Basic examples of strong monads

--- implicit-monads/StrongMonads.v
+++ implicit-monads/StrongMonads.v
@@ -1,4 +1,6 @@
 (** Monads with stronger hypothesis to simplify composition *)
+Require Import String.
+
 Set Implicit Arguments.
 
 Class Monad : Type := {
@@ -8,13 +10,55 @@ Class Monad : Type := {
   M : Type -> Type :=
     fun A => I -> (A + E) * O;
   strong_ret : I -> O;
-  strong_bind : I -> O -> I;
+  strong_bind : O -> I;
   ret : forall A, A -> M A :=
     fun A x i =>
       (inl x, strong_ret i);
   bind : forall A B, M A -> (A -> M B) -> M B :=
     fun A B x f i =>
       match x i with
-      | (inl x', o) => f x' (strong_bind i o)
+      | (inl x', o) => f x' (strong_bind o)
       | (inr e, o) => (inr e, o)
       end}.
+
+Instance Id : Monad := {
+  I := unit;
+  E := Empty_set;
+  O := unit;
+  strong_ret := fun _ => tt;
+  strong_bind := fun _ => tt}.
+
+Instance Option : Monad := {
+  I := unit;
+  E := unit;
+  O := unit;
+  strong_ret := fun _ => tt;
+  strong_bind := fun _ => tt}.
+
+Instance Error (E : Type) : Monad := {
+  I := unit;
+  E := E;
+  O := unit;
+  strong_ret := fun _ => tt;
+  strong_bind := fun _ => tt}.
+
+Instance Print : Monad := {
+  I := list string;
+  E := Empty_set;
+  O := list string;
+  strong_ret := fun i => i;
+  strong_bind := fun o => o}.
+
+Instance State (S : Type) : Monad := {
+  I := S;
+  E := Empty_set;
+  O := S;
+  strong_ret := fun i => i;
+  strong_bind := fun o => o}.
+
+Instance Loop : Monad := {
+  I := nat;
+  E := unit;
+  O := nat;
+  strong_ret := fun i => i;
+  strong_bind := fun o => o}.
