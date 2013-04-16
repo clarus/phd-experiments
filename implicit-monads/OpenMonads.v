13e25fb5caac7812b5b0fbdcf1d67d9011b89928
Guillaume Claret <guillaume@claret.me>
2013-04-16 19:47:31 +0200

    Definition of coroutines

--- implicit-monads/OpenMonads.v
+++ implicit-monads/OpenMonads.v
@@ -1,5 +1,8 @@
 (** Monads with an open structure to give more freedom to the run operations *)
 Require Import String.
+Require Import List.
+
+Import ListNotations.
 
 Set Implicit Arguments.
 
@@ -39,11 +42,25 @@ Fixpoint bind {m : Monad} A B (x : M.t A) (f : A -> M.t B) : M.t B :=
     | (inr x, o) => (inr (bind x f), o)
     end).
 
+Definition seq {m : Monad} A (x : M.t unit) (f : M.t A) : M.t A :=
+  bind x (fun _ => f).
+
 Fixpoint run {m : Monad} A (x : M.t A) (I_of_O : O -> I) (i : I) : (A + E) * O :=
   match (M.open x) i with
   | (inl xe, o) => (xe, o)
   | (inr x, o) => run x I_of_O (I_of_O o)
   end.
+
+Fixpoint run_with_break {m : Monad} A (x : M.t A) (I_of_O : O -> option I) : M.t A :=
+  M.new (fun i =>
+    match (M.open x) i with
+    | (inl xe, o) => (inl xe, o)
+    | (inr x, o) =>
+      match I_of_O o with
+      | Some i' => (M.open (run_with_break x I_of_O)) i'
+      | None => (inr x, o)
+      end
+    end).
 
 Definition combine (m1 m2 : Monad) : Monad := {|
   I := @I m1 * @I m2;
@@ -54,6 +71,16 @@ Definition combine (m1 m2 : Monad) : Monad := {|
     (O_of_I i1, O_of_I i2)|}.
 
 Infix "++" := combine.
+
+Fixpoint gret {m m' : Monad} A (x : @M.t m' A) : @M.t (m ++ m') A :=
+  M.new (m := m ++ m') (fun i =>
+    let (i1, i2) := i in
+    let o1 := O_of_I i1 in
+    match (M.open x) i2 with
+    | (inl (inl x'), o2) => (inl (inl x'), (o1, o2))
+    | (inl (inr e), o2) => (inl (inr (inr e)), (o1, o2))
+    | (inr x', o2) => (inr (gret x'), (o1, o2))
+    end).
 
 Instance Option : Monad := {
   I := unit;
@@ -93,3 +120,63 @@ Instance Loop : Monad := {
   E := unit;
   O := nat;
   O_of_I := fun i => i}.
+
+(** Coroutines *)
+Instance Waiter (m : Monad) (A B : Type) : Monad := {
+  I := A -> @M.t m B;
+  E := Empty_set;
+  O := option (A -> @M.t m B);
+  O_of_I := fun i => Some i}.
+
+Module Coroutine.
+  Definition t {m : Monad} (A B T : Type) := @M.t (Waiter m A B ++ m) T.
+  
+  (*Definition new {m : Monad} A B (x : @M.t (Waiter A ++ m) B) : t A B :=
+    x.*)
+  
+  Definition forget {m : Monad} A B : t A B unit :=
+    M.new (m := Waiter m A B ++ m) (fun i =>
+      let (_, i_m) := i in
+      (inl (inl tt), (None, O_of_I i_m))).
+  
+  Definition use_on {m : Monad} A B (a : A) : t A B B :=
+    M.new (m := Waiter m A B ++ m) (fun i =>
+      let (f, i_m) := i in
+      match (M.open (f a)) i_m with
+      | (inl (inl y), o_m) => (inl (inl y), (Some f, o_m))
+      | (inl (inr y), o_m) => (inl (inr (inr y)), (Some f, o_m))
+      | (inr y, o_m) => (inr (gret y), (Some f, o_m))
+      end).
+  
+  Definition yield {m : Monad} A B (a : A) : t A B B :=
+    seq (forget _ _) (use_on _ a).
+  
+  Fixpoint force {m : Monad} A B T (x : t A B T) (f : A -> M.t B) : M.t (T + t A B T) :=
+    M.new (fun i_m =>
+      match (M.open x) (f, i_m) with
+      | (inl (inl x'), (_, o_m)) => (inl (inl (inl x')), o_m)
+      | (inl (inr (inr e)), (_, o_m)) => (inl (inr e), o_m)
+      | (inl (inr (inl e)), _) => match e with end
+      | (inr x', (Some f, o_m)) => (inr (force x' f), o_m)
+      | (inr x', (None, o_m)) => (inl (inl (inr x')), o_m)
+      end).
+  
+  Fixpoint terminate {m : Monad} A B T (x : t A B T) (f : A -> M.t B) : M.t T :=
+    M.new (fun i_m =>
+      match (M.open x) (f, i_m) with
+      | (inl (inl x'), (_, o_m)) => (inl (inl x'), o_m)
+      | (inl (inr (inr e)), (_, o_m)) => (inl (inr e), o_m)
+      | (inl (inr (inl e)), _) => match e with end
+      | (inr x', (_, o_m)) => (inr (terminate x' f), o_m)
+      end).
+End Coroutine.
+
+Fixpoint iter_list {m : Monad} A (l : list A) : Coroutine.t A unit unit :=
+  match l with
+  | nil => ret tt
+  | x :: l' => seq (Coroutine.yield _ x) (iter_list l')
+  end.
+
+
+
+
