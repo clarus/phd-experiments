9a6394256cc7b9d534aef7bcd18e0e0420d52a0f
Guillaume Claret <guillaume@claret.me>
2013-05-02 17:05:31 +0200

    Common monads and partial_run

--- implicit-monads/ConcreteMonads.v
+++ implicit-monads/ConcreteMonads.v
@@ -1,4 +1,8 @@
 (** Something incomplete but simple to get a first concrete result *)
+Require Import List.
+
+Import ListNotations.
+
 Set Implicit Arguments.
 
 Module Result.
@@ -71,4 +75,78 @@ Definition combine_assoc_left (m1 m2 m3 : Monad) A (x : @M ((m1 ++ m2) ++ m3) A)
           end in
         ((o1, (o2, o3)), Err e)
       end
+    end.
+
+Definition combine_assoc_right (m1 m2 m3 : Monad) A (x : @M (m1 ++ (m2 ++ m3)) A)
+  : @M ((m1 ++ m2) ++ m3) A :=
+  fun i =>
+    match i with
+    | ((i1, i2), i3) =>
+      match x (i1, (i2, i3)) with
+      | ((o1, (o2, o3)), Val x) => (((o1, o2), o3), Val x)
+      | ((o1, (o2, o3)), Err e) =>
+        let e := match e with
+          | inl e1 => inl (inl e1)
+          | inr (inl e2) => inl (inr e2)
+          | inr (inr e3) => inr e3
+          end in
+        (((o1, o2), o3), Err e)
+      end
+    end.
+
+Definition gret {m m' : Monad} A (x : @M m' A) : @M (m ++ m') A :=
+  fun i =>
+    let (i1, i2) := i in
+    match x i2 with
+    | (o2, Val x) => ((i1, o2), Val x)
+    | (o2, Err e) => ((i1, o2), Err (inr e))
+    end.
+
+Instance Option : Monad := {
+  S := unit;
+  E := unit}.
+
+Definition option_none A : @M Option A :=
+  fun _ => (tt, Err tt).
+
+Definition option_run A (x : @M Option A) : option A :=
+  match x tt with
+  | (_, Val x) => Some x
+  | (_, Err _) => None
+  end.
+
+Instance Error (E : Type) : Monad := {
+  S := unit;
+  E := E}.
+
+Definition raise E A (e : E) : @M (Error E) A :=
+  fun _ => (tt, Err e).
+
+Instance Print (A : Type) : Monad := {
+  S := list A;
+  E := Empty_set}.
+
+Definition print A (x : A) : @M (Print A) unit :=
+  fun i => (x :: i, Val tt).
+
+Instance State (S : Type) : Monad := {
+  S := S;
+  E := Empty_set}.
+
+Definition read (S : Type) : @M (State S) S :=
+  fun s => (s, Val s).
+
+Definition write (S : Type) (x : S) : @M (State S) unit :=
+  fun _ => (x, Val tt).
+
+Instance Loop : Monad := {
+  S := nat;
+  E := unit}.
+
+Definition partial_run {m m' : Monad} A (x : @M (m ++ m') A) (i_m : @S m)
+  : @M (Error (@E m) ++ m') A :=
+  fun i =>
+    let (_, i_m') := i in
+    match x (i_m, i_m') with
+    | ((_, o_2), r) => ((tt, o_2), r)
     end.
