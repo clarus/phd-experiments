486518ccdae41159b74588f361e158fcf5c978cb
Guillaume Claret <guillaume@claret.me>
2013-05-15 19:04:54 +0200

    Logical world separated from the concrete state

--- typed-assembly/ClosureLess.v
+++ typed-assembly/ClosureLess.v
@@ -4,30 +4,57 @@ Import ListNotations.
 
 Set Implicit Arguments.
 
-Module Body.
-  Inductive t : Type -> Type -> Type :=
-  | op : forall H H' H'', (H -> H') -> t H' H'' -> t H H''
-  | jcc : forall H H', (H -> bool) -> t H H' -> t H H' -> t H H'
-  | call : forall A B H H' H'', (H -> A) -> (B -> H') -> t A B -> t H' H'' -> t H H''
-  | ret : forall H, t H H.
+Module Asm.
+  Inductive t (S : Set) (L : S -> Type) : Type :=
+  | op :
+    forall (f : S -> S), (forall s, L s -> L (f s)) ->
+    t L -> t L
+  | jcc :
+    (S -> bool) -> (forall s, L s -> L s) ->
+    t L -> t L -> t L
+  | call :
+    forall (fx : S -> S), (forall s, L s -> L (fx s)) ->
+    forall (fy : S -> S), (forall s, L s -> L (fy s)) ->
+    t L -> t L -> t L
+  | ret :
+    t L.
   
-  Fixpoint eval H H' (b : t H H') (i : H) {struct b} : H'.
-    destruct b as [H H' H'' f b | H H' c b1 b2 | A B H H' H'' x y f b | H].
-      exact (eval _ _ b (f i)).
+  Fixpoint eval (S : Set) (L : S -> Type) (a : t L) (s : S) (l : L s) {struct a} : {s : S & L s}.
+    destruct a as [fs fl n | c fl nnc n | xs xl ys yl f n | ].
+      exact (
+        let l := fl s l in
+        let s := fs s in
+        eval _ _ n s l).
       
-      exact (if c i then eval _ _ b1 i else eval _ _ b2 i).
+      exact (
+        let l := fl s l in
+        if c s then
+          eval _ _ n s l
+        else
+          let (s, l) := eval _ _ nnc s l in
+          eval _ _ n s l).
       
-      exact (eval _ _ b (y (eval _ _ f (x i)))).
+      exact (
+        let l := xl s l in
+        let s := xs s in
+        let (s, l) := eval _ _ f s l in
+        let l := yl s l in
+        let s := ys s in
+        eval _ _ n s l).
       
-      exact i.
+      exact (existT _ s l).
   Defined.
-End Body.
+End Asm.
 
-Import Body.
+Import Asm.
+Require Import Arith.Compare_dec.
 
-Definition test1 : t unit nat :=
-  op (fun _ => 12) (
-  op (fun n => n + 1) (
-  ret _)).
+Definition test1 : t (fun n => 3 <= n).
+  refine (
+    op (fun _ => 12) (fun n _ => _) (
+    op (fun n => n + 1) (fun n h => _) (
+    ret _)));
+  auto with *.
+Defined.
 
-Compute eval test1 tt.
+Compute eval test1 3 (leb_complete 3 3 eq_refl).
