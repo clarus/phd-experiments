84738d718c022369649a78051318bab05bb29b9e
Guillaume Claret <guillaume@claret.me>
2013-06-05 18:18:03 +0200

    Compilation to the macro assembler

--- typed-assembly/StaticCode.v
+++ typed-assembly/StaticCode.v
@@ -146,17 +146,19 @@ Module ArithLang.
         abstract (unfold y; omega).
       Defined.
       
+      (** For x >= 10, (x {>= 10} + 12 {= 12}) {>= 20}  *)
       Definition e (x : Z) (H : x >= 10) : t (fun y => y >= 20).
         refine (
           plus _ (const (fun x => x >= 10) x H) (const _ 12 eq_refl) _).
         abstract (intros; omega).
       Defined.
       
-      Lemma ge15 : 15 >= 10.
-        omega.
+      Lemma geb_complete (m n : Z) :
+        (if Z_ge_dec m n then true else false) = true -> m >= n.
+        now destruct (Z_ge_dec m n).
       Qed.
       
-      Compute projT1 (eval (e ge15)).
+      Compute projT1 (eval (e (geb_complete 15 10 eq_refl))).
     End Test.
   End Source.
   
@@ -176,23 +178,23 @@ Module ArithLang.
       | const : forall (P : Z -> Prop), {z : Z | P z} -> t
       | uminus : forall (P1 P : Z -> Prop), (forall z1, P1 z1 -> P (- z1)) -> t
       | plus : forall (P1 P2 P : Z -> Prop), (forall z1 z2, P1 z1 -> P2 z2 -> P (z1 + z2)) -> t
-      (*| times : forall (P1 P2 P : Z -> Prop), (forall z1 z2, P1 z1 -> P2 z2 -> P (z1 * z2)) -> t*).
+      | times : forall (P1 P2 P : Z -> Prop), (forall z1 z2, P1 z1 -> P2 z2 -> P (z1 * z2)) -> t.
       
       Definition input_sig (i : t) (context : Sig.t) : Sig.t :=
         match i with
         | const _ _ => context
         | uminus P1 _ _ => P1 :: context
-        | plus P1 P2 _ _ => P1 :: P2 :: context
+        | plus P1 P2 _ _ | times P1 P2 _ _ => P1 :: P2 :: context
         end.
       
       Definition output_sig (i : t) (context : Sig.t) : Sig.t :=
         match i with
-        | const P _ | uminus _ P _ | plus _ _ P _ => P :: context
+        | const P _ | uminus _ P _ | plus _ _ P _ | times _ _ P _ => P :: context
         end.
       
       Definition eval (i : t) (context : Sig.t) (s : Stack.t (input_sig i context))
         : Stack.t (output_sig i context).
-        destruct i as [P z | P1 P Hcast | P1 P2 P Hcast]; simpl in *.
+        destruct i as [P z | P1 P Hcast | P1 P2 P Hcast | P1 P2 P Hcast]; simpl in *.
         - exact (Stack.cons P z s).
         
         - inversion_clear s as [|P' z1 sig s'].
@@ -202,6 +204,10 @@ Module ArithLang.
         - inversion_clear s as [|P' z1 sig s']; inversion_clear s' as [|P' z2 sig s''].
           destruct z1 as (z1, H1); destruct z2 as (z2, H2).
           exact (Stack.cons P (existT _ (z1 + z2) (Hcast z1 z2 H1 H2)) s'').
+        
+        - inversion_clear s as [|P' z1 sig s']; inversion_clear s' as [|P' z2 sig s''].
+          destruct z1 as (z1, H1); destruct z2 as (z2, H2).
+          exact (Stack.cons P (existT _ (z1 * z2) (Hcast z1 z2 H1 H2)) s'').
       Defined.
     End Instr.
     
@@ -225,6 +231,33 @@ Module ArithLang.
       Definition eval P (p : t P []) : {z : Z | P z} :=
         eval_aux p Stack.nil.
     End Program.
+    
+    Module Compile.
+      Import Instr Program.
+      
+      Fixpoint compile_aux context P P' (e : Source.t P) (k : Program.t P' (P :: context))
+        : Program.t P' context.
+        destruct e as [P z H | P1 P e1 Hcast | P1 P2 P e1 e2 Hcast | P1 P2 P e1 e2 Hcast].
+        - exact (cons (const P (existT _ z H)) context k).
+        
+        - exact (
+          compile_aux _ P1 P' e1 (
+          cons (uminus P1 P Hcast) _ k)).
+        
+        - exact (
+          compile_aux _ P2 P' e2 (
+          compile_aux _ P1 P' e1 (
+          cons (plus P1 P2 P Hcast) _ k))).
+        
+        - exact (
+          compile_aux _ P2 P' e2 (
+          compile_aux _ P1 P' e1 (
+          cons (times P1 P2 P Hcast) _ k))).
+      Defined.
+      
+      Definition compile P (e : Source.t P) : Program.t P [] :=
+        compile_aux e (Program.nil _).
+    End Compile.
     
     Module Test.
       Import Instr Program.
@@ -252,6 +285,11 @@ Module ArithLang.
       Compute projT1 (eval p1).
       Compute projT1 (eval p2).
       Compute projT1 (eval p3).
+      
+      Definition p4 := Compile.compile (Source.Test.e
+        (Source.Test.geb_complete 15 10 eq_refl)).
+      
+      Compute projT1 (eval p4).
     End Test.
   End MacroAsm.
   
