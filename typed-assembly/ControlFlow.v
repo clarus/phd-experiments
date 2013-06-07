74c23ff59b602d0e61040facd1c9fb20ba82da14
Guillaume Claret <guillaume@claret.me>
2013-06-07 15:21:12 +0200

    Beginning of a CFG with the concept of shape

--- typed-assembly/ControlFlow.v
+++ typed-assembly/ControlFlow.v
@@ -9,7 +9,7 @@ Module WellFounded.
     Hwf : well_founded lt}.
 End WellFounded.
 
-Module Context.
+(*Module Context.
   Module T.
     Inductive t (S : Set) : Type :=
     | nil
@@ -46,26 +46,64 @@ Module Context.
     intro x.
     constructor; intros y H.
   Admitted.
+End Context.*)
+
+(** The shape of positions of the loop entry points *)
+Module Shape.
+  Inductive t : Type :=
+  | nil
+  | loop (A : Type) (WF : WellFounded.t A) (shape_in shape_next : t)
+  | jcc (shape_true shape_false shape_next : t).
+End Shape.
+
+(** A state of the program's loop variants. This type has a well-founded order. *)
+Module Path.
+  Inductive t : Shape.t -> Type :=
+  | nil :
+    t Shape.nil
+  | loop_in : forall A WF shape_in shape_next (a : A) (path : t shape_in),
+    t (Shape.loop (A := A) WF shape_in shape_next)
+  | loop_next : forall A WF shape_in shape_next (a : A) (path : t shape_next),
+    t (Shape.loop (A := A) WF shape_in shape_next)
+  | jcc_true : forall shape_true shape_false shape_next (path : t shape_true),
+    t (Shape.jcc shape_true shape_false shape_next)
+  | jcc_false : forall shape_true shape_false shape_next (path : t shape_false),
+    t (Shape.jcc shape_true shape_false shape_next)
+  | jcc_next : forall shape_true shape_false shape_next (path : t shape_next),
+    t (Shape.jcc shape_true shape_false shape_next).
+End Path.
+
+(** The stack of the loop entry points a program is depending on. *)
+Module Context.
+  Inductive t (S : Set) : Type :=
+  | nil
+  | cons (A : Type) (WF : WellFounded.t A) (L : S -> Type) (context : t S).
+  
+  Fixpoint append S (C1 C2 : t S) : t S :=
+    match C1 with
+    | nil _ => C2
+    | cons WF L C1 => cons WF L (append C1 C2)
+    end.
 End Context.
 
 Module CFG.
-  Inductive t (S : Set) : Context.t S -> (S -> Type) -> (S -> Type) -> Type :=
-  | nop : forall C L,
-    t C L L
-  | op : forall C Lin L Lout,
+  Inductive t (S : Set) : Context.t S -> Shape.t -> (S -> Type) -> (S -> Type) -> Type :=
+  | nop : forall context L,
+    t context Shape.nil L L
+  | op : forall context shape Lin L Lout,
     (forall s, {s' : S & Lin s -> L s'}) ->
-    t C L Lout ->
-    t C Lin Lout
-  | jcc : forall C Lin Lin_true Lin_false Lmerge Lout,
+    t context shape L Lout ->
+    t context shape Lin Lout
+  | jcc : forall context shape_true shape_false shape_next
+      Lin Lin_true Lin_false Lmerge Lout,
     (forall s, (Lin s -> Lin_true s) + (Lin s -> Lin_false s)) ->
-    t C Lin_true Lmerge -> t C Lin_false Lmerge -> t C Lmerge Lout ->
-    t C Lin Lout
-  | label : forall C Lin L Lout (WF : WellFounded.t (sigT Lin)),
-    t (Context.cons WF C) Lin L -> t C L Lout ->
-    t C Lin Lout
-  | goto : forall C1 C2 Lin L Lout (WF : WellFounded.t (sigT Lin)),
-    
-    t (Context.append C1 (Context.cons WF C2)) Lin Lout.
+    t context shape_true Lin_true Lmerge -> t context shape_false Lin_false Lmerge -> t context shape_next Lmerge Lout ->
+    t context (Shape.jcc shape_true shape_false shape_next) Lin Lout
+  | loop : forall context shape_in shape_next Lin L Lout A WF,
+    t (Context.cons (A := A) WF Lin context) shape_in Lin L -> t context shape_next L Lout ->
+    t context (Shape.loop (A := A) WF shape_in shape_next) Lin Lout
+  | goto : forall context1 A WF context2 Lin L Lout,
+    t (Context.append context1 (Context.cons (A := A) WF L context2)) Shape.nil Lin Lout.
   
   (*Inductive t (S : Set) : (S -> Type) -> (S -> Type) -> Type :=
   | op : forall (f : S -> S) (Lin L Lout : S -> Type)
