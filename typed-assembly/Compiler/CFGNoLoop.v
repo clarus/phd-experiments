1fe1f2141c6c80f7246ffeea277cb3ba3e460c8b
Guillaume Claret <guillaume@claret.me>
2013-06-14 17:07:49 +0200

    Compilation from CFG to Linear abandonned (left in comments)

--- typed-assembly/Compiler/CFGNoLoop.v
+++ typed-assembly/Compiler/CFGNoLoop.v
@@ -44,7 +44,7 @@ Module CFG.
     match g with
     | nop _ => 0
     | op _ _ g => 1 + (length g)
-    | jcc _ _ g_true g_false g => length g_true + length g_false + length g
+    | jcc _ _ g_true g_false g => 1 + length g_true + length g_false + length g
     end.
   
   Module Test.
@@ -68,9 +68,202 @@ Module CFG.
   End Test.
 End CFG.
 
-Module Compile.
-  Require Import Arith.Wf_nat.
+(*Module Compile.
+  Require Import Arith.
   Import CFG.
+  
+  (*Module AnnotedProgram.
+    Inductive t (S : Set) (Lin Lout : S -> Type) : Type :=
+    | new (L : nat -> S -> Type).
+    
+    Fixpoint concat (S : Set) (L1 L2 L3 : S -> Type) (p1 : t L1 L2) (p2 : t L2 L3)
+  End AnnotedProgram.*)
+  
+  Definition f_lt T size (x : {ip : nat & T ip}) : nat :=
+      if leb size (projT1 x) then
+        0
+      else
+        size - projT1 x.
+  
+  Definition lt (S : Set) (L : nat -> S -> Type) (size : nat) : Instr.lt_type L :=
+    fun x y =>
+      f_lt _ size x < f_lt _ size y.
+  
+  Lemma lt_wf (S : Set) (L : nat -> S -> Type) size : well_founded (lt L size).
+    apply well_founded_ltof.
+  Defined.
+  
+  (*Definition lift_L (S : Set) (Lpre Lpost L : nat -> S -> Type) (ip0 length : nat)
+    (ip : nat) : S -> Type :=
+    match nat_compare ip ip0 with
+    | Lt => Lpre ip
+    | _ =>
+      match nat_compare ip (ip0 + length) with
+      | Lt => L ip
+      | _ => Lpost ip
+      end
+    end.*)
+  
+  Definition lift_L (S : Set) (Lin Lout : S -> Type) (L : nat -> S -> Type) (ip0 length : nat)
+    (ip : nat) : S -> Type :=
+    if beq_nat ip ip0 then
+      Lin
+    else if beq_nat ip (ip0 + length) then
+      Lout
+    else
+      L ip.
+  
+  Fixpoint compile_aux (S : Set) (Lin Lout : S -> Type) (g : CFG.t Lin Lout) (ip0 : nat)
+    : { L : nat -> S -> Type & Program.t (lt (lift_L Lin Lout L ip0 (length g)) (length g)) ip0 }.
+    destruct g as
+      [ L
+      | Lin L Lout f g
+      | Lin Lin_true Lin_false Lmerge Lout f g_true g_false g].
+    - exists (fun _ => L).
+      exact (Program.nil _ _).
+    
+    - destruct (compile_aux _ _ _ g (1 + ip0)) as (Lg, p).
+      exists (fun ip =>
+        if beq_nat ip (1 + ip0) then
+          L
+        else
+          Lg ip).
+      refine (Program.cons _ _).
+      + intro s.
+        exists (1 + ip0).
+        destruct (f s) as (s', fl).
+        exists s'.
+        unfold lt, f_lt; simpl.
+        intro l.
+        unfold lift_L in l.
+        rewrite (proj2 (beq_nat_true_iff ip0 ip0) eq_refl) in l.
+        constructor.
+        * unfold lift_L.
+          assert (H1 : (Datatypes.S ip0) <> ip0); [auto |].
+          rewrite (proj2 (beq_nat_false_iff _ _) H1).
+          rewrite (proj2 (beq_nat_true_iff _ _) eq_refl).
+          destruct g; rewrite plus_comm; simpl.
+            rewrite (proj2 (beq_nat_true_iff _ _) eq_refl).
+            exact (fl l).
+            
+            assert (H2 : ip0 <> (Datatypes.S (length g + ip0)));
+              [apply succ_plus_discr |].
+            rewrite (proj2 (beq_nat_false_iff _ _) H2).
+            exact (fl l).
+            
+            assert (H2 : ip0 <> (Datatypes.S (length g1 + length g2 + length g3 + ip0)));
+              [apply succ_plus_discr |].
+            rewrite (proj2 (beq_nat_false_iff _ _) H2).
+            exact (fl l).
+        
+        * destruct (le_lt_dec (length g) ip0) as [Hle | Hlt].
+            rewrite (leb_correct _ _ Hle).
+            destruct ip0.
+intuition. destruct ip0; simpl; auto.
+          destruct ip0.
+intuition.
+            rewrite <- minus_n_O; auto.
+            
+            
+            
+intuition.
+            
+intuition. 
+            rewrite plus_comm.
+          refine (let size := length g in _).
+          inversion size.
+        
+        
+        assert (l' := fl l).
+        
+        unfold lift_L at 1.
+        assert (H : (Datatypes.S ip0) <> ip0); [auto |].
+        rewrite (proj2 (beq_nat_false_iff _ _) H).
+        
+        unfold lift_L.
+        induction ip0.
+        * simpl.
+          intro l.
+          unfold 
+        
+        induction ip0; intro l; simpl.
+        
+        unfold lift_L at 1.
+        rewrite (proj2 (beq_nat_true_iff ip0 ip0) eq_refl).
+        intro l.
+        unfold lift_L at 1.
+        assert (H : (1 + ip0) <> ip0); [auto |].
+        simpl in l.
+        assert (l' := fl l).
+        rewrite (proj2 (beq_nat_false_iff (1 + ip0) ip0) H).
+        
+        exists (fl l).
+      simpl.
+        if beq_nat ip ip0 then
+          Lin
+        else
+          Lg ip).
+  
+  Fixpoint compile_aux (S : Set) (Lin Lout : S -> Type) (g : CFG.t Lin Lout) (ip0 : nat)
+    (Lpre Lpost : nat -> S -> Type) {struct g}
+    : { L : nat -> S -> Type & { length : nat & { p : Program.t (lt (lift_L Lpre Lpost L ip0 length)) ip0 |
+      length = Program.length p /\ L ip0 = Lin /\ L (ip0 + length) = Lout }}}.
+    destruct g as
+      [ L
+      | Lin L Lout f g
+      | Lin Lin_true Lin_false Lmerge Lout f g_true g_false g].
+    - exists (fun _ => L).
+      exists 0.
+      exists (Program.nil _ _).
+      tauto.
+    
+    - refine (let Lpre' := fun ip => if beq_nat ip ip0 then Lin else Lpre ip in _).
+      destruct (compile_aux _ _ _ g (1 + ip0) Lpre' Lpost) as (Lg, (length, (p, (Hlength, (HLout, HLpost))))).
+      exists (fun ip => if leb ip ip0 then Lin else Lg ip).
+      exists (1 + length).
+      refine (let i : Instr.t (lt ) ip0 := _ in _).
+    
+    
+    
+    
+    
+    - exists (fun ip =>
+        match nat_compare ip ip0 with
+        | Lt => Lpre ip
+        | Eq => L
+        | Gt => Lpost ip
+        end).
+      exists (Program.nil _ _).
+      repeat split; simpl; try rewrite <- plus_n_O.
+      + intros ip H.
+        now rewrite (proj1 (nat_compare_lt _ _) H).
+      
+      + now rewrite (proj2 (nat_compare_eq_iff _ _) eq_refl).
+      
+      + now rewrite (proj2 (nat_compare_eq_iff _ _) eq_refl).
+      
+      + intros ip H.
+        now rewrite (proj1 (nat_compare_gt _ _) H).
+    
+    - refine (let Lpre' := fun ip => if beq_nat ip ip0 then Lin else Lpre ip in _).
+      destruct (compile_aux _ _ _ g (1 + ip0) Lpre' Lpost) as (Lg, (p, (HLpre, (HL, (HLout, HLpost))))).
+      exists Lg.
+      refine (let i : Instr.t (lt Lg) ip0 := _ in _).
+        intro s.
+        exists (1 + ip0).
+        destruct (f s) as (s', fl).
+        exists s'.
+        intro l.
+        assert (Lg ip0 = Lin).
+          assert (H := HLpre ip0 (lt_n_Sn _)).
+          unfold Lpre' in H.
+          now rewrite <- beq_nat_refl in H.
+        rewrite H in l.
+        refine (let l' := fl l in _).
+        rewrite HL.
+      exists (Program.cons () p).
+      exists (fun ip => if leb ip ip0 then Lin else Lg ip).
+      exists (Program.cons () p).
   
   Fixpoint Laux (S : Set) (Lin Lout : S -> Type) (g : CFG.t Lin Lout)
     (ip : nat) : (S -> Type) + nat :=
@@ -103,19 +296,6 @@ Module Compile.
     | inr _ => Lout
     end.
   
-  Definition lt (S : Set) (L : nat -> S -> Type) : Instr.lt_type L :=
-    fun x y =>
-    projT1 x < projT1 y.
-  
-  Lemma lt_wf (S : Set) (L : nat -> S -> Type) : well_founded (lt L).
-    apply well_founded_ltof.
-  Defined.
-  
   (*Fixpoint compile (S : Set) (Lin Lout : S -> Type) (g : CFG.t Lin Lout)
     : Program.t (lt (L g)) 0.*)
-End Compile.
-
-
-
-
-
+End Compile.*)
