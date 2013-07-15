0ddb241bc5e4f5e55908ebb2fc96a43cccf8e249
Guillaume Claret <guillaume@claret.me>
2013-07-15 16:55:49 +0200

    Translation from PHOAS to indexed variables

---
+++ typed-assembly/Source/BareLowSource.v
@@ -0,0 +1,51 @@
+(** Low-level source language using indexed variables. *)
+Require Import List.
+Require Import Memory.
+Require Import LowSource.
+
+Set Implicit Arguments.
+Import ListNotations.
+
+Module Operand.
+  Inductive t : Set :=
+  | imm : Value.t -> t
+  | var : nat -> t.
+  
+  Definition of_phoas (op : LowSource.Operand.t nat) : t :=
+    match op with
+    | LowSource.Operand.imm _ v => imm v
+    | LowSource.Operand.var x => var x
+    end.
+End Operand.
+
+Module Source.
+  Inductive t : Set :=
+  | final : Operand.t -> t
+  | op : BinOp.t -> Operand.t -> Operand.t -> t -> t.
+  
+  Definition of_phoas (s : forall T, LowSource.Source.t T) : t :=
+    let fix aux s i :=
+      match s with
+      | LowSource.Source.final x => final (Operand.of_phoas x)
+      | LowSource.Source.op o x y s => op o (Operand.of_phoas x) (Operand.of_phoas y) (aux (s i) (S i))
+      end in
+    aux (s nat) 0.
+End Source.
+
+Module Example.
+  Module OfPhoas.
+    Import LowSource.Operand.
+    Import LowSource.Source.
+    
+    Definition ex1 T : t T :=
+      final (imm _ (Value.bits [true])).
+    
+    Definition ex2 T : t T :=
+      op BinOp.add (imm _ (Value.bits [true])) (imm _ (Value.bits [false])) (fun x =>
+      op BinOp.and (imm _ (Value.bits [false; true])) (imm _ (Value.bits [])) (fun y =>
+      final (var x))).
+    
+    Compute Source.of_phoas ex1.
+    Compute Source.of_phoas ex2.
+  End OfPhoas.
+End Example.
---
+++ typed-assembly/Source/LowSource.v
@@ -0,0 +1,24 @@
+(** Low-level source language. *)
+Require Import List.
+Require Import Memory.
+
+Set Implicit Arguments.
+Import ListNotations.
+
+Module BinOp.
+  Inductive t : Set :=
+  | add | sub | mul
+  | not | and | xor.
+End BinOp.
+
+Module Operand.
+  Inductive t (T : Type) : Type :=
+  | imm : Value.t -> t T
+  | var : T -> t T.
+End Operand.
+
+Module Source.
+  Inductive t (T : Type) : Type :=
+  | final : Operand.t T -> t T
+  | op : BinOp.t -> Operand.t T -> Operand.t T -> (T -> t T) -> t T.
+End Source.
