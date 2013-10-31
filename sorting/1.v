2f743d62a45c5fa58926a1049bd5fee283232565
Guillaume Claret <dev@clarus.me>
2013-10-31 17:00:12 +0100

    Rewriting of the beginning with Program

--- sorting/1.v
+++ sorting/1.v
@@ -1,21 +1,23 @@
 (** A bubble sort implementation. *)
 Require Import List.
 Require Import Omega.
+Require Import Program.
 
 Set Implicit Arguments.
 
 Module Sig.
+  Definition new := exist.
   Definition val := proj1_sig.
   Definition spec := proj1_sig.
 End Sig.
 
 Module List.
   Module Index.
-    Record t A (l : list A) : Set := new {
-      i : nat;
-      spec : i < length l}.
+    Definition t A (l : list A) := {i : nat | i < length l}.
     
-    Definition from_new_list A (l l' : list A)
+    Definition new A (l : list A) i H : t l := exist _ i H.
+    
+    (*Definition from_new_list A (l l' : list A)
       (i : t l) (H : length l = length l') : t l'.
       refine (new l' (i := Index.i i) _).
       destruct i.
@@ -24,27 +26,74 @@ Module List.
     
     Definition dup A (l : list A) (i : t l)
       B (l' : list B) H : t l':=
-      new l' (i := Index.i i) H.
+      new l' (i := Index.i i) H.*)
   End Index.
   
-  Fixpoint read A (l : list A) (i : Index.t l) : A.
-    destruct i as [i Hi].
-    destruct l as [|x l].
-    - simpl in Hi.
-      clear read; abstract omega.
-    - destruct i as [|i].
-      + exact x.
-      + refine (read _ l (Index.new l (i := i) _)).
-        simpl in Hi.
-        clear read; abstract omega.
-  Defined.
+  Ltac destruct_index_omega :=
+    match goal with
+    | [i : Index.t _ |- _] =>
+      destruct i; simpl in *;
+      omega
+    end.
   
-  Fixpoint write A (l : list A) (i : Index.t l) (x : A) {struct l} : list A.
-    admit.
-  Defined.
+  Program Fixpoint read A (l : list A) (i : Index.t l) : A :=
+    match l with
+    | nil => !
+    | cons x l =>
+      match i with
+      | O => x
+      | S i => read i
+      end
+    end.
+  Next Obligation.
+    destruct_index_omega.
+  Qed.
+  Next Obligation.
+    destruct_index_omega.
+  Qed.
   
-  Axiom write_keeps_length : forall A (l : list A) (i : Index.t l) (x : A),
+  Program Fixpoint write A (l : list A) (i : Index.t l) (x : A) : list A :=
+    match l with
+    | nil => !
+    | cons x' l =>
+      match i with
+      | O => cons x l
+      | S i => cons x (write i x)
+      end
+    end.
+  Next Obligation.
+    destruct_index_omega.
+  Qed.
+  Next Obligation.
+    destruct_index_omega.
+  Qed.
+  
+  Lemma write_keeps_length : forall A (l : list A) (i : Index.t l) (x : A),
     length (write i x) = length l.
+    intros A l i x.
+    induction l; simpl.
+    - destruct_index_omega.
+    - destruct i as [i]; destruct i; simpl; auto.
+  Qed.
+  
+  Program Definition write_modifies_lemma : Prop :=
+    forall A (l : list A) (i : Index.t l) (x : A),
+      read (Index.new (i := i) (write i x) _) = x.
+  Next Obligation.
+    rewrite write_keeps_length.
+    now destruct i.
+  Qed.
+  
+  Lemma write_modifies : write_modifies_lemma.
+    intros A l i x.
+    induction l; simpl.
+    - destruct_index_omega.
+    - destruct i as [i]; destruct i; simpl; trivial.
+      (* TODO *)
+  Qed.
+  
+  Program Lemma write_modifies : forall A (l : list A) (i : Index.t l) (x : A),
+    read (Sig.new _ (Sig.val i) _ : Index.t (write i x)) = x.
   
   Definition write_modifies_lemma : Prop.
     refine (forall A (l : list A) (i : Index.t l) (x : A),
