b5e9ff7da9049fbedc8b5ea06b4ee451d0a26e1e
Guillaume Claret <dev@clarus.me>
2014-06-23 12:03:20 +0200

    Event module

--- composable-monads/Main.v
+++ composable-monads/Main.v
@@ -114,22 +114,22 @@ Fixpoint combine_assoc_right (m1 m2 m3 : Monad) A (x : @C.t (m1 ++ m2 ++ m3) A)
       end
     end).*)
 
-Fixpoint lift_state S1 S2 E A (x : C.t S2 E A) : @C.t (S1 * S2) E A :=
+Fixpoint lift_state S1 S2 E A (x : C.t S1 E A) : @C.t (S1 * S2) E A :=
   C.make (fun (s : S1 * S2) =>
     let (s1, s2) := s in
-    match C.open x s2 with
-    | (Val x, s2) => (Val x, (s1, s2))
-    | (Err e, s2) => (Err e, (s1, s2))
-    | (Mon x, s2) => (Mon (lift_state _ x), (s1, s2))
+    match C.open x s1 with
+    | (Val x, s1) => (Val x, (s1, s2))
+    | (Err e, s1) => (Err e, (s1, s2))
+    | (Mon x, s1) => (Mon (lift_state _ x), (s1, s2))
     end).
 
-Fixpoint lift_error S E E' A (x : C.t S E A) : @C.t S (E + E') A :=
+(*Fixpoint lift_error S E E' A (x : C.t S E A) : @C.t S (E + E') A :=
   C.make (fun s =>
     match C.open x s with
     | (Val x, s) => (Val x, s)
     | (Err e, s) => (Err (inl e), s)
     | (Mon x, s) => (Mon (lift_error _ x), s)
-    end).
+    end).*)
 
 Module Option.
   Definition none A : C.t unit unit A :=
@@ -141,10 +141,12 @@ Module Error.
     C.make (fun _ => (Err e, tt)).
 End Error.
 
-Module Print.
-  Definition print A (x : A) : C.t (list A) Empty_set unit :=
+Module Log.
+  Definition t := list.
+  
+  Definition log A (x : A) : C.t (t A) Empty_set unit :=
     C.make (fun s => (Val tt, x :: s)).
-End Print.
+End Log.
 
 Module State.
   Definition read (S : Type) : C.t S Empty_set S :=
@@ -196,39 +198,39 @@ End Entropy.
 Module Concurrency.
   (** Executes [x] and [y] concurrently, using a boolean stream as source of entropy. *)
   Fixpoint par S E A B
-    (x : C.t (Entropy.t * S) E A) (y : C.t (Entropy.t * S) E B) {struct x}
-    : C.t (Entropy.t * S) E (A * B) :=
-    let fix par_aux y {struct y} : C.t (Entropy.t * S) E (A * B) :=
-      C.make (fun (s : Entropy.t * S) =>
+    (x : C.t (S * Entropy.t) E A) (y : C.t (S * Entropy.t) E B) {struct x}
+    : C.t (S * Entropy.t) E (A * B) :=
+    let fix par_aux y {struct y} : C.t (S * Entropy.t) E (A * B) :=
+      C.make (fun (s : S * Entropy.t) =>
         match s with
-        | (Streams.Cons b bs, s) =>
+        | (s, Streams.Cons b bs) =>
           if b then
-            let (r, ss) := C.open x (bs, s) in
+            let (r, ss) := C.open x (s, bs) in
             (match r with
             | Val x => Mon (let! y := y in ret (x, y))
             | Err e => Err e
             | Mon x => Mon (par x y)
             end, ss)
           else
-            let (r, ss) := C.open y (bs, s) in
+            let (r, ss) := C.open y (s, bs) in
             (match r with
             | Val y => Mon (let! x := x in ret (x, y))
             | Err e => Err e
             | Mon y => Mon (par_aux y)
             end, ss)
         end) in
-    C.make (fun (s : Entropy.t * S) =>
+    C.make (fun (s : S * Entropy.t) =>
       match s with
-      | (Streams.Cons b bs, s) =>
+      | (s, Streams.Cons b bs) =>
         if b then
-          let (r, ss) := C.open x (bs, s) in
+          let (r, ss) := C.open x (s, bs) in
           (match r with
           | Val x => Mon (let! y := y in ret (x, y))
           | Err e => Err e
           | Mon x => Mon (par x y)
           end, ss)
         else
-          let (r, ss) := C.open y (bs, s) in
+          let (r, ss) := C.open y (s, bs) in
           (match r with
           | Val y => Mon (let! x := x in ret (x, y))
           | Err e => Err e
@@ -245,30 +247,65 @@ Module Concurrency.
       end).
 End Concurrency.
 
+Module List.
+  Fixpoint iter_seq S E A (f : A -> C.t S E unit) (l : list A) : C.t S E unit :=
+    match l with
+    | [] => ret tt
+    | x :: l =>
+      let! _ := f x in
+      iter_seq f l
+    end.
+  
+  Fixpoint iter_par S E A (f : A -> C.t (S * Entropy.t) E unit) (l : list A)
+    : C.t (S * Entropy.t) E unit :=
+    match l with
+    | [] => ret tt
+    | x :: l =>
+      let! _ := Concurrency.par (f x) (iter_par f l) in
+      ret tt
+    end.
+End List.
+
+Module Event.
+  Definition t := list.
+  
+  Definition loop_seq S E A (f : A -> C.t S E unit) : C.t (S * t A) E unit :=
+    C.make (fun (s : S * t A) =>
+      let (s, events) := s in
+      (Mon (lift_state (t A) (List.iter_seq f events)), (s, []))).
+  
+  Definition loop_par S E A (f : A -> C.t (S * Entropy.t) E unit)
+    : C.t (S * Entropy.t * t A) E unit :=
+    C.make (fun (s : S * Entropy.t * t A) =>
+      let (s, events) := s in
+      (Mon (lift_state (t A) (List.iter_par f events)), (s, []))).
+End Event.
+
 Module Test.
   Definition eval_seq (x : C.t (list nat) Empty_set unit) :=
     snd (eval x []).
   
-  Definition eval_par (x : C.t (Entropy.t * list nat) Empty_set unit) (e : Entropy.t) :=
-    snd (snd (eval x (e, []))).
+  Definition eval_par (x : C.t (list nat * Entropy.t) Empty_set unit) (e : Entropy.t) :=
+    snd (snd (eval x ([], e))).
   
+  (** Two threads are printing concurrently two lists of numbers. *)
   Module PrintList.
-    Fixpoint print_before (n : nat) : C.t (list nat) Empty_set unit :=
+    Fixpoint print_before (n : nat) : C.t (Log.t nat) Empty_set unit :=
       match n with
       | O => ret tt
       | S n =>
-        let! _ := Print.print n in
+        let! _ := Log.log n in
         print_before n
       end.
     
-    Definition two_prints_seq (n : nat) : C.t (list nat) Empty_set unit :=
+    Definition two_prints_seq (n : nat) : C.t (Log.t nat) Empty_set unit :=
       let! _ := print_before n in
       print_before (2 * n).
     
-    Definition print_before_par (n : nat) : C.t (Entropy.t * list nat) Empty_set unit :=
+    Definition print_before_par (n : nat) : C.t (Log.t nat * Entropy.t) Empty_set unit :=
       lift_state (Entropy.t) (print_before n).
     
-    Definition two_prints_par (n : nat) : C.t (Entropy.t * list nat) Empty_set unit :=
+    Definition two_prints_par (n : nat) : C.t (Log.t nat * Entropy.t) Empty_set unit :=
       let! _ := Concurrency.par (print_before_par n) (print_before_par (2 * n)) in
       ret tt.
     
@@ -282,34 +319,48 @@ Module Test.
     Compute eval_par (two_prints_par 12) (Entropy.random 0).
   End PrintList.
   
+  (** A list of threads are printing a number each. *)
   Module ListOfPrints.
-    Fixpoint iter_seq S E A (f : A -> C.t S E unit) (l : list A)
-      : C.t S E unit :=
-      match l with
-      | [] => ret tt
-      | x :: l =>
-        let! _ := f x in
-        iter_seq f l
-      end.
+    Definition print_seq_seq (n k : nat) : C.t (Log.t nat) Empty_set unit :=
+      List.iter_seq (Log.log (A := nat)) (List.seq n k).
     
-    Fixpoint iter_par S E A (f : A -> C.t (Entropy.t * S) E unit) (l : list A)
-      : C.t (Entropy.t * S) E unit :=
-      match l with
-      | [] => ret tt
-      | x :: l =>
-        let! _ := Concurrency.par (f x) (iter_par f l) in
-        ret tt
-      end.
+    Definition print_seq_par (n k : nat) : C.t (Log.t nat * Entropy.t) Empty_set unit :=
+      List.iter_par (fun n => lift_state _ (Log.log n)) (List.seq n k).
     
-    Definition print_seq_seq (n k : nat) : C.t (list nat) Empty_set unit :=
-      iter_seq (Print.print (A := nat)) (List.seq n k).
-    
-    Definition print_seq_par (n k : nat) : C.t (Entropy.t * list nat) Empty_set unit :=
-      iter_par (fun n => lift_state _ (Print.print n)) (List.seq n k).
-    
-    Compute eval_seq (print_seq_seq 10 10).
-    Compute eval_par (print_seq_par 10 10) Entropy.left.
-    Compute eval_par (print_seq_par 10 10) Entropy.right.
-    Compute eval_par (print_seq_par 10 10) (Entropy.random 12).
+    Compute eval_seq (print_seq_seq 10 20).
+    Compute eval_par (print_seq_par 10 20) Entropy.left.
+    Compute eval_par (print_seq_par 10 20) Entropy.right.
+    Compute eval_par (print_seq_par 10 20) (Entropy.random 12).
   End ListOfPrints.
+  
+  Module Events.
+    Module UiInput.
+      Inductive t : Set :=
+      | Add : string -> t
+      | Remove : nat -> t.
+    End UiInput.
+    
+    Module UiOutput.
+      Definition t := list string.
+    End UiOutput.
+    
+    Module ServerInput.
+      Definition t := list string.
+    End ServerInput.
+    
+    Module ServerOutput.
+      Definition t := list string.
+    Module ServerOutput.
+    
+    Module Model.
+      Definition t := list string.
+    End Model.
+    
+    Parameter handle_ui : UiInput.t -> C.t (Log.t UiOutput.t * Model.t) Empty_set unit.
+    Parameter handle_server : ServerInput.t -> C.t (Log.t ServerOutput.t * Model.t) Empty_set unit.
+    
+    (*Definition todo :=
+      let! _ := Concurrency.par (Event.loop_par (fun  => )) in
+      ret tt.*)
+  End Events.
 End Test.
